aa302b448a6e27e109216c1b74a07688d5bafc2b
lukiee555 <sonilokesh24@gmail.com>
2020-09-09 17:33:19 +0530

    Array > Leetcode > Non Decreasing Array

---
+++ Array/Leetcode/non_decreasing_array.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    bool checkPossibility(vector<int>& arr) {      
+        int count = 0;
+        for(int i = 1; i < arr.size(); i++){
+            if(arr[i] < arr[i - 1]){
+                if(i == 1 || arr[i - 2] <= arr[i]){
+                    arr[i - 1] = arr[i];
+                    count++;
+                }else{
+                    arr[i] = arr[i - 1];
+                    count++;
+                }
+            }
+        }
+        return count <=1;
+    }
+};
