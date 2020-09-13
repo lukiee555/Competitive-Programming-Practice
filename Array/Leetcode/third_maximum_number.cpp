aa29aa4ae3c74e6f077321348a88d7fed8ba3c47
lukiee555 <sonilokesh24@gmail.com>
2020-09-13 09:31:13 +0530

    Array > Leetcode > Third Maximum Number

---
+++ Array/Leetcode/third_maximum_number.cpp
@@ -0,0 +1,13 @@
+// Problem Link :- https://leetcode.com/problems/third-maximum-number/
+class Solution {
+public:
+    int thirdMax(vector<int>& nums) {
+        set<int> top3;
+        for (auto num : nums) {
+            top3.insert(num);
+            if (top3.size() > 3)
+                top3.erase(top3.begin());
+        }
+     return top3.size() == 3 ? *top3.begin() : *top3.rbegin();
+    }
+};
