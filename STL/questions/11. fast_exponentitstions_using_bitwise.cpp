5b23a17004637ad08fbf2331ad78d08c5a48fcd3
lukiee555 <sonilokesh24@gmail.com>
2020-11-09 12:00:40 +0530

    STL >> Questions >> Fast exponentitation using bitwise operators

---
+++ STL/questions/11. fast_exponentitstions_using_bitwise.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+#include<algorithm>
+using namespace std;
+
+int power_optimised(int a, int n){
+    int ans = 1;
+    while (n > 0)
+    {
+        int last_bit = (n & 1);
+        if(last_bit){
+            ans = a * a;
+        }
+        a = a*a;
+        n = n >> 1;
+
+    }
+    return ans;
+    
+}
+
+int main(){
+    int n;
+    cin>>n;
+    cout<<power_optimised(n);
+    return 0;
+}
