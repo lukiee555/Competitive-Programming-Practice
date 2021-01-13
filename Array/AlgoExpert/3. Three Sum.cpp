70dcc1b73abda145c7b34dd588ca46dcbeb0b3c0
lukiee555 <sonilokesh24@gmail.com>
2021-01-13 17:58:20 +0530

    Array >> AlgoExpert >> Three Sum

--- Array/AlgoExpert/3. Three Sum.cpp
+++ Array/AlgoExpert/3. Three Sum.cpp
@@ -1,29 +1,35 @@
-class Solution {
+class Solution
+{
 public:
-    vector<vector<int>> threeSum(vector<int>& nums) {
-        sort(nums.begin(),nums.end());
+    vector<vector<int>> threeSum(vector<int> &nums)
+    {
+        sort(nums.begin(), nums.end());
         vector<vector<int>> output;
-        for (int i = 0; i < nums.size(); ++i) {
-        // Never let i refer to the same value twice to avoid duplicates.
-        if (i != 0 && nums[i] == nums[i - 1]) 
-            continue;
-        int j = i + 1;
-        int k = nums.size() - 1;
-        while (j < k) {
-            if (nums[i] + nums[j] + nums[k] == 0) {
-                output.push_back({nums[i], nums[j], nums[k]});
-                ++j;
-             // Never let j refer to the same value twice (in an output) to avoid                      duplicates
-                while (j < k && nums[j] == nums[j-1]) ++j;
-            } 
-            else if (nums[i] + nums[j] + nums[k] < 0) {
-                ++j;
-            } 
-            else {
-            --k;
+        for (int i = 0; i < nums.size(); ++i)
+        {
+            if (i != 0 && nums[i] == nums[i - 1])
+                continue;
+            int j = i + 1;
+            int k = nums.size() - 1;
+            while (j < k)
+            {
+                if (nums[i] + nums[j] + nums[k] == 0)
+                {
+                    output.push_back({nums[i], nums[j], nums[k]});
+                    ++j;
+                    while (j < k && nums[j] == nums[j - 1])
+                        ++j;
+                }
+                else if (nums[i] + nums[j] + nums[k] < 0)
+                {
+                    ++j;
+                }
+                else
+                {
+                    --k;
+                }
             }
         }
-    }
-    return output;
+        return output;
     }
 };
