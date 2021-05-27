abf4f05ef3d0c1087f238a5e4ab56acd90fd3b08
lukiee555 <sonilokesh24@gmail.com>
2021-05-27 17:34:18 +0530

    Array >> InterviewBit >> Rotate Matrix

---
+++ Array/InterviewBit/9. Rotate Matrix.cpp
@@ -0,0 +1,28 @@
+void Solution::rotate(vector<vector<int> > &A) {
+    int i,j,t;
+    //Transoose
+    for(i=0;i<A.size();i++)
+    {
+        for(j=i;j<A[i].size();j++)
+        {
+            t=A[i][j];
+            A[i][j]=A[j][i];
+            A[j][i]=t;
+        }
+    }
+    //Flip for clockwise flip row and anticlock wise flip columns
+    i=0;j=A[0].size()-1;
+    while(i<j)
+    {
+        t=0;
+        while(t<A.size())
+        {
+            int temp=A[t][i];
+            A[t][i]=A[t][j];
+            A[t][j]=temp;
+            t++;
+        }
+        i++;
+        j--;
+    }
+}
