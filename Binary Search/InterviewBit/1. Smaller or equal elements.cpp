56d8569f8e0cb835211f98286a4d8e617143c4ec
lukiee555 <sonilokesh24@gmail.com>
2021-06-12 23:08:49 +0530

    Binary Search >> InterviewBit >> Smaller or equal elements

---
+++ Binary Search/InterviewBit/1. Smaller or equal elements.cpp
@@ -0,0 +1,11 @@
+int Solution::solve(vector<int> &A, int B) {
+    int start = 0, end = A.size(), mid = 0;
+    
+    while(start <= end) {
+        mid = (start + end) / 2;
+        if(A[mid] <= B) start = mid + 1;
+        else end = mid - 1;
+    }
+    
+    return min((int)A.size(), mid + (A[mid] <= B));
+}
