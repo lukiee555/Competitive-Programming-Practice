3d4ff3fbe1bd2755cb497a2f91bae865d6bacced
lukiee555 <sonilokesh24@gmail.com>
2021-06-18 20:05:33 +0530

    Linked List >> InterviewBit >> Swap List Nodes in pairs

---
+++ Linked List/InterviewBit/6. Swap List Nodes in pairs.cpp
@@ -0,0 +1,19 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+ListNode* Solution::swapPairs(ListNode* A) {
+    ListNode *temp=A;
+    while(temp!=NULL&&temp->next!=NULL){
+        int t=temp->val;
+        temp->val=temp->next->val;
+        temp->next->val=t;
+        
+        temp=temp->next->next;
+    }
+    return A;
+}
