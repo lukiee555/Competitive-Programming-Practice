30c8c7c897bb43b9776e69970e6d2d2f06d6293e
lukiee555 <sonilokesh24@gmail.com>
2021-06-19 13:23:15 +0530

    Linked List >> InterviewBit >> Sort List

---
+++ Linked List/InterviewBit/8. Sort List.cpp
@@ -0,0 +1,83 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     struct ListNode *next;
+ * };
+ * 
+ * typedef struct ListNode listnode;
+ * 
+ * listnode* listnode_new(int val) {
+ *     listnode* node = (listnode *) malloc(sizeof(listnode));
+ *     node->val = val;
+ *     node->next = NULL;
+ *     return node;
+ * }
+ */
+/**
+ * @input A : Head pointer of linked list 
+ * 
+ * @Output head pointer of list.
+ */
+ listnode* merge(listnode* A, listnode* B){
+    if(A == NULL){
+        return B;
+    }
+    if(B == NULL){
+        return A;
+    }
+    
+    listnode* head = NULL;
+    
+    if(A->val < B->val){
+        head = A;
+        A = A->next;
+    }
+    else{
+        head = B;
+        B = B->next;
+    }
+    
+    listnode* temp = head;
+    
+    while(A != NULL && B != NULL){
+        if(A->val < B->val){
+            temp->next = A;
+            A = A->next;
+        }
+        else{
+            temp->next = B;
+            B = B->next;
+        }
+        temp = temp->next;
+    }
+    
+    if(A != NULL){
+        temp->next = A;
+    }
+    else{
+        temp->next = B;
+    }
+    
+    return head;
+} 
+listnode* sortList(listnode* A) {
+    listnode* head = A;
+    
+    if(head == NULL || head->next == NULL){
+        return head;
+    }
+    
+    listnode* start = A;
+    listnode* end = A->next;
+    
+    while(end != NULL && end->next != NULL){
+        start = start->next;
+        end = (end->next)->next;
+    }
+    
+    end = start->next;
+    start->next = NULL;
+    
+    return merge(sortList(head), sortList(end)); 
+}
