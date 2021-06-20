46560d9c2d20ea49a9069fbeb64041fe502580d9
lukiee555 <sonilokesh24@gmail.com>
2021-06-20 16:10:07 +0530

    Linked List >> InterviewBit >> List Cycle

---
+++ Linked List/InterviewBit/9. List Cycle.cpp
@@ -0,0 +1,54 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     struct ListNode *next;
+ * };
+ * 
+ * typedef struct ListNode listnode;
+ * 
+ * listnode* listnode_new(int val) {
+ *     listnode* node = (listnode *) malloc(sizeof(listnode));
+ *     node->val = val;
+ *     node->next = NULL;
+ *     return node;
+ * }
+ */
+/**
+ * @input A : Head pointer of linked list 
+ * 
+ * @Output head pointer of list.
+ */
+listnode* detectCycle(listnode* A) {
+    if(A == NULL || A->next == NULL){
+        return NULL;
+    }
+    
+    listnode* slow = A;
+    listnode* fast = A;
+    
+    while(slow != NULL && fast != NULL){
+        slow = slow->next;
+        if(fast->next == NULL){
+            return NULL;
+        }
+        else{
+            fast = (fast->next)->next;
+        }
+        if(slow == fast){
+            break;
+        }
+    }
+    
+    if(slow == NULL || fast == NULL){
+        return NULL;
+    }
+    
+    listnode* curr = A;
+    while(curr != slow){
+        curr = curr->next;
+        slow = slow->next;
+    }
+    
+    return curr;
+}
