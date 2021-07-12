b273d1128400de9590f9c8430fb9047f2396ceb6
lukiee555 <sonilokesh24@gmail.com>
2021-07-12 06:41:39 +0530

    Linked List >> InterviewBit >> Swap Nodes in Pairs Recursive

--- Linked List/LeetCode/2. Swap Nodes in Pairs.cpp
+++ Linked List/LeetCode/2. Swap Nodes in Pairs.cpp
@@ -11,22 +11,39 @@
 class Solution {
 public:
     ListNode* swapPairs(ListNode* head) {
-        ListNode* newHead = new ListNode(0);
-        newHead->next = head;
-        ListNode* prev = newHead;
-        ListNode* current, *forward, *temp;
-        while(prev->next != NULL && prev->next->next != NULL){
-            current = prev->next;
-            forward = current->next;
-            temp = forward->next;
+//         ListNode* newHead = new ListNode(0);
+//         newHead->next = head;
+//         ListNode* prev = newHead;
+//         ListNode* current, *forward, *temp;
+//         while(prev->next != NULL && prev->next->next != NULL){
+//             current = prev->next;
+//             forward = current->next;
+//             temp = forward->next;
             
-            prev->next = forward;
-            forward->next = current;
-            current->next = temp;
+//             prev->next = forward;
+//             forward->next = current;
+//             current->next = temp;
             
-            prev = current;
+//             prev = current;
             
+//         }
+//         return newHead->next;
+        
+        //Recursive Solution
+        ListNode* temp;
+        if(head == NULL )
+            return NULL;
+        else if(head->next == NULL){
+            return head;
         }
-        return newHead->next;
+        else{
+            temp = head->next;
+            head->next = temp->next;
+            temp->next = head;
+            head = temp;
+            head->next->next = swapPairs(head->next->next);
+            return head;
+        }
+            
     }
 };
