55f984d14ca12d4076ae004d2a4b5f2898ececac
lukiee555 <sonilokesh24@gmail.com>
2021-07-24 10:45:12 +0530

    Stack >> InterviewBit >> Simplofy Directory Path

---
+++ Stack/InterviewBit/5. Simplify Directory Path.cpp
@@ -0,0 +1,13 @@
+string Solution::simplifyPath(string A) {
+    string res, tmp;
+    vector<string> stk;
+    stringstream ss(A);
+    while(getline(ss,tmp,'/')) {
+        if (tmp == "" or tmp == ".") continue;
+        if (tmp == ".." and !stk.empty()) stk.pop_back();
+        else if (tmp != "..") stk.push_back(tmp);
+    }
+    for(auto str : stk) res += "/"+str;
+    return res.empty() ? "/" : res;
+    
+}
