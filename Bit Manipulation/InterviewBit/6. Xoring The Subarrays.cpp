e52388342a7e76b9e5593efbae25c3e8829f1af7
lukiee555 <sonilokesh24@gmail.com>
2021-07-26 18:56:49 +0530

    Bit Manipulation >> InterviewBit >> Xoring The Subarrays

---
+++ Bit Manipulation/InterviewBit/6. Xoring The Subarrays.cpp
@@ -0,0 +1,10 @@
+int Solution::solve(vector<int> &A) {
+    if(A.size() % 2 == 0)
+        return 0;
+    else{
+        int ans = 0;
+        for(int i = 0; i < A.size(); i+=2)
+            ans = ans ^ A[i];
+        return ans;
+    }
+} 
