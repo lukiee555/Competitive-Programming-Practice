1a278b4adcb507cd7a1acc95e0e21971e5c8dcea
lukiee555 <sonilokesh24@gmail.com>
2021-08-02 00:09:43 +0530

    Tree >> Leetcode >> Validate BST

---
+++ Tree/Leetcode/4. Validate Binary Search Tree.cpp
@@ -0,0 +1,53 @@
+//Recursive TC = O(n) 100ms Solution
+class isBSTReturn{
+    public:
+    bool isBST;
+    long minimum;
+    long maximum;
+};
+class Solution {
+public:
+    isBSTReturn isValidBSTHelper(TreeNode* root){
+        if(root == NULL){
+            isBSTReturn out;
+            out.isBST = true;
+            out.minimum = LONG_MAX;
+            out.maximum = LONG_MIN;
+            return out;
+        }
+        isBSTReturn  leftOut = isValidBSTHelper(root->left);
+        isBSTReturn  rightOut = isValidBSTHelper(root->right);
+        
+        isBSTReturn out;
+        out.minimum = min((long)root->val, min(leftOut.minimum, rightOut.minimum));
+        out.maximum = max((long)root->val, max(leftOut.maximum, rightOut.maximum));
+        out.isBST = (((long)root->val > leftOut.maximum) && ((long)root->val < rightOut.minimum) &&
+                    leftOut.isBST && rightOut.isBST);
+        return out;
+        
+    }
+    bool isValidBST(TreeNode* root) {
+        return isValidBSTHelper(root).isBST;
+    }
+};
+//Recursive TC = O(nh)
+// class Solution {
+// public:
+//     long maxTree(TreeNode* root){
+//         if(root == NULL)
+//             return LONG_MIN;
+//         return max((long)root->val, max(maxTree(root->left), maxTree(root->right)));
+//     }
+//     long minTree(TreeNode* root){
+//         if(root == NULL)
+//             return LONG_MAX;
+//         return min((long)root->val, min(minTree(root->left), minTree(root->right)));
+//     }
+//     bool isValidBST(TreeNode* root) {
+//         if(root == NULL)
+//             return true;
+//         long leftMax = maxTree(root->left);
+//         long rightMin = minTree(root->right);
+//         return ((long)root->val > leftMax && (long)root->val < rightMin && isValidBST(root->left) && isValidBST(root->right));
+//     }
+// };
