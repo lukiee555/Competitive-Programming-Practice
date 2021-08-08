fdd99fd79e43f4a862c7694e6f5caad1375b8703
lukiee555 <sonilokesh24@gmail.com>
2021-08-08 18:46:34 +0530

    Tree >> Leetcode >> Invert Binary Tree

--- Tree/Leetcode/10. Invert Binary Tree.cpp
+++ Tree/Leetcode/10. Invert Binary Tree.cpp
@@ -1,3 +1,20 @@
+// Non Recursive
+TreeNode* invertTree(TreeNode* root) {
+    std::stack<TreeNode*> stk;
+    stk.push(root);
+    
+    while (!stk.empty()) {
+        TreeNode* p = stk.top();
+        stk.pop();
+        if (p) {
+            stk.push(p->left);
+            stk.push(p->right);
+            std::swap(p->left, p->right);
+        }
+    }
+    return root;
+}
+//Recursive
 class Solution {
 public:
     TreeNode* invertTree(TreeNode* root) {
@@ -10,4 +27,5 @@ public:
         }
         return root;
     }
+
 };
